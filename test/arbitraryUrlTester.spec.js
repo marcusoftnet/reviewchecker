6d62f73aa810dc90c39046601d5f229a283dce69
Marcus Hammarberg <marcusoft.net@gmail.com>
2016-09-30 14:56:46 +0200

    Final tweaks in place - deploying

--- test/arbitraryUrlTester.spec.js
+++ test/arbitraryUrlTester.spec.js
@@ -4,38 +4,42 @@ const co = require("co");
 
 describe("Some tests I've done for selectors and urls", function () {
 
-	it("Kanban In Action at Amazon", function (done) {
+	var testReviewData = { url : "", selector: "", prefix: "", suffix: "" };
+
+	function testOneReview(reviewData, done) {
 		co(function *() {
-			let oneReview = {
-				url: 'https://www.amazon.com/Kanban-Action-Marcus-Hammarberg/dp/1617291056/',
-				selector: '.arp-rating-out-of-text',
-				prefix: 'Kanban in Action',
-				suffix: ''
-			};
-
-			let result = yield reviewFetcher.getReviewData(oneReview);
+			let result = yield reviewFetcher.getReviewData(reviewData);
 			result.should.not.be.empty;
-
 			console.log(result);
-
 		}).then(done, done);
+	};
+
+	it("Kanban In Action at Amazon", function (done) {
+			testReviewData.url =
+				'https://www.amazon.com/Kanban-Action-Marcus-Hammarberg/dp/1617291056/';
+			testReviewData.selector = '.arp-rating-out-of-text';
+
+			testOneReview(testReviewData, done);
+	});
+
+	it("Kanban In Action on GoodReads", function (done) {
+		testReviewData.url = "https://www.goodreads.com/book/show/17789383-kanban-in-action";
+		testReviewData.selector = ".average";
+
+		testOneReview(testReviewData, done);
 	});
 
 	it("npm course at PluralSight", function (done) {
-		co(function *() {
-			let oneReview = {
-				url: "https://app.pluralsight.com/library/courses/npm-build-tool-introduction/table-of-contents",
-				selector: ".rating__raters-count",
-				prefix: "Introduction to npm as a Build Tool:",
-				suffix: "ratings"
-			};
+		testReviewData.url = "http://app.pluralsight.com/author/marcus-hammarberg";
+		testReviewData.selector = "body > div.ng-scope > div:nth-child(2) > section.push-down > div > div > div:nth-child(2) > div > div.columns.large-12 > div:nth-child(2) > div.large-2.columns > span > input[type='hidden']";
 
-			let result = yield reviewFetcher.getReviewData(oneReview);
-			result.should.not.be.empty;
+		testOneReview(testReviewData, done);
+	});
 
-			console.log(result);
-
-		}).then(done, done);
+	it("SpecFlow.Assists.Dynamic on NuGet", function (done) {
+		testReviewData.url = "https://www.nuget.org/packages/SpecFlow.Assist.Dynamic/";
+		testReviewData.selector = "#stats > p.stat-number";
+		testOneReview(testReviewData, done);
 	});
 
 });
