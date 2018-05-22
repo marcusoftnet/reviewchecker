f6a1dbde6b365733b779c9a38fa1a3efee00c951
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-05-22 21:20:28 +0200

    Refactored index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-'use strict';
-const co = require('co');
-const ApiBuilder = require('claudia-api-builder');
-const api = new ApiBuilder();
-const reviewFetcher = require('./lib/reviewFetcher.js');
+const ApiBuilder = require('claudia-api-builder')
+const api = new ApiBuilder()
+const reviewFetcher = require('./lib/reviewFetcher.js')
 
-api.get('/', function () {
-	return reviewFetcher.getReviewKeys();
-});
+api.get('/', () => { return reviewFetcher.getReviewKeys() })
 
-api.get('/{key}', function (request) {
-	return co(function *() {
-		let key = request.pathParams.key;
-		let results = yield reviewFetcher.getReviewData(key);
-		return results;
-  });
-});
+api.get('/{key}', async (request) => {
+  return reviewFetcher.getReviewData(request.pathParams.key)
+})
 
-module.exports = api;
+module.exports = api
